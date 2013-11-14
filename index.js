68a870620da9ef00791f199cad5f5382410a96a1
David Konsumer <konsumer@jetboystudio.com>
2013-11-14 00:21:20 -0800

    this self

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ var e = 'http://tuner.pandora.com/services/json/';
 var partners = require('./partners.json');
 
 var Pandora = function(partner, cb){
-  this.call('auth.partnerLogin', partners[partner], function(er, data){
+  var self = this;
+  self.call('auth.partnerLogin', partners[partner], function(er, data){
     if (er) return cb(er);
-    this.token = data.result.partnerAuthToken;
+    self.token = data.result.partnerAuthToken;
     cb(null, data.result);
   });
 }
