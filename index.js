3c7c3e2127fee88ee384e0d4e133e7a11ca4fc59
David Konsumer <konsumer@jetboystudio.com>
2013-11-14 00:18:25 -0800

    start of structure

--- index.js
+++ index.js
@@ -6,5 +6,19 @@ var e = 'http://tuner.pandora.com/services/json/';
 
 var partners = require('./partners.json');
 
-// get partner session credentials
-r.post(e + '?method=auth.partnerLogin')
+var Pandora = function(partner, cb){
+  this.call('auth.partnerLogin', partners[partner], function(er, data){
+    if (er) return cb(er);
+    this.token = data.result.partnerAuthToken;
+    cb(null, data.result);
+  });
+}
+
+Pandora.prototype.call = function(method, params, cb){
+   r.post(e + '?method=' + method, params, data);
+};
+
+
+module.exports = Pandora;
+
+
